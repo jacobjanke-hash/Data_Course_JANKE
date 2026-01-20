638cd0273725bf071d5d49d71c83f81507be9ef3
Data Course Student <student@example.com>
2026-01-20 16:28:58 +0000

    5th commit - Added first R practice script

---
+++ Work_In_Progress/test_script.R
@@ -0,0 +1,15 @@
+# My first R script for Data Course
+# This script demonstrates basic R operations
+
+# Print a message
+print("Hello, Data Course!")
+
+# Basic arithmetic
+x <- 10
+y <- 20
+sum_result <- x + y
+print(paste("The sum of", x, "and", y, "is", sum_result))
+
+# Create a simple vector
+numbers <- c(1, 2, 3, 4, 5)
+print(paste("Mean of numbers:", mean(numbers)))
